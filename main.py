536ffd411adc00b31a2c75658b802e64cd3ffbed
RomanMainAcc <romanborisuk0@gmail.com>
2023-07-10 22:09:24 +0300

    second commit

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import csv
 import json
 
-with open('csv_data.csv', 'r') as file:
-    csv_data = csv.DictReader(file)
 
-    data = []
-    for row in csv_data:
-        data.append(row)
-    # print(data)
+def convert_csv_to_json(csv_file, json_file):
+    with open(csv_file, 'r') as file:
+        csv_data = csv.DictReader(file)
 
-with open('json_data.json', 'w') as file:
-    json.dump(data, file, indent=4)
+        # data = []
+        # for row in csv_data:
+        #     data.append(row)
+        # print(data)
+        data = [row for row in csv_data]
+
+    with open(json_file, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+csv_file = 'csv_data.csv'
+json_file = 'json_data.json'
+
+convert_csv_to_json(csv_file, json_file)
